404c86bd2097e2af14b9927b2b2e856d59bad35f
davidfernandez <david.fernandez@pal-robotics.com>
2017-04-25 16:38:20 +0200

    Allow multiple Tiagos on a single Gazebo

--- tiago_gazebo/scripts/grasp_demo.py
+++ tiago_gazebo/scripts/grasp_demo.py
@@ -8,11 +8,11 @@ from sensor_msgs.msg import JointState
 if __name__ == "__main__":
   rospy.init_node("grasp_demo")
   rospy.loginfo("Waiting for play_motion...")
-  client = actionlib.SimpleActionClient("/play_motion", PlayMotionAction)
+  client = actionlib.SimpleActionClient("play_motion", PlayMotionAction)
   client.wait_for_server()
   rospy.loginfo("...connected.")
 
-  rospy.wait_for_message("/joint_states", JointState)
+  rospy.wait_for_message("joint_states", JointState)
   rospy.sleep(3.0)
 
   rospy.loginfo("Grasping demo...")
@@ -33,7 +33,7 @@ if __name__ == "__main__":
   client.send_goal(goal)
   client.wait_for_result(rospy.Duration(10.0))
 
-  rospy.sleep(1.0) 
+  rospy.sleep(1.0)
 
   goal.motion_name = 'pick_demo'
   goal.skip_planning = False
