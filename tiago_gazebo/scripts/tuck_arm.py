f5eb60805484b2636fd1d2e6dd81efad46d05287
Jordan Palacios <jordan.palacios@pal-robotics.com>
2021-07-02 10:56:41 +0200

    Migrated tuck_arm.py to ros2

--- tiago_gazebo/scripts/tuck_arm.py
+++ tiago_gazebo/scripts/tuck_arm.py
@@ -1,25 +1,72 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import rospy
-import actionlib
-from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
-from sensor_msgs.msg import JointState
+# Copyright 2021 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-if __name__ == "__main__":
-  rospy.init_node("tuck_arm")
-  rospy.loginfo("Waiting for play_motion...")
-  client = actionlib.SimpleActionClient("play_motion", PlayMotionAction)
-  client.wait_for_server()
-  rospy.loginfo("...connected.")
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
 
-  rospy.wait_for_message("joint_states", JointState)
-  rospy.sleep(3.0)
+from play_motion_msgs.action import PlayMotion
 
-  rospy.loginfo("Tuck arm...")
-  goal = PlayMotionGoal()
-  goal.motion_name = 'home'
-  goal.skip_planning = False
 
-  client.send_goal(goal)
-  client.wait_for_result(rospy.Duration(10.0))
-  rospy.loginfo("Arm tucked.")
+class PlayMotionActionClient(Node):
+
+    def __init__(self):
+        super().__init__('play_motion_action_client')
+        self._action_client = ActionClient(self, PlayMotion, 'play_motion')
+
+    def send_goal(self, motion_name, skip_planning):
+        goal_msg = PlayMotion.Goal()
+        goal_msg.motion_name = motion_name
+        goal_msg.skip_planning = skip_planning
+
+        self._action_client.wait_for_server()
+
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+
+        if not goal_handle.accepted:
+            self.get_logger().error('Goal rejected')
+            return
+
+        self.get_logger().info('Goal accepted')
+
+        self._get_result_future = goal_handle.get_result_async()
+
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: error code ({}): {}'.format(
+            result.error_code, result.error_string))
+        rclpy.shutdown()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    action_client = PlayMotionActionClient()
+
+    action_client.send_goal('home', True)
+
+    rclpy.spin(action_client)
+
+
+if __name__ == '__main__':
+    main()
