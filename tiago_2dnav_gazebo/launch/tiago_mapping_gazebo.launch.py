140045b36e966d3d9d47e3a6cf8550a0e35a41a0
cescfolch <cesc.folch@pal-robotics.com>
2021-10-20 16:15:55 +0200

    Add navigation and mapping to ros2

---
+++ tiago_2dnav_gazebo/launch/tiago_mapping_gazebo.launch.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2021 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch_pal.include_utils import include_launch_py_description
+
+
+def generate_launch_description():
+    # Create the launch description and populate
+    ld = LaunchDescription([
+        include_launch_py_description(
+            'tiago_gazebo', ['launch', 'tiago_gazebo.launch.py'],
+            launch_arguments={
+                'world_name': 'pal_office',
+                }.items()),
+        include_launch_py_description(
+            'tiago_2dnav', ['launch', 'tiago_nav_bringup.launch.py'],
+            launch_arguments={
+                'slam': 'True'
+                }.items()),
+    ])
+
+    return ld
