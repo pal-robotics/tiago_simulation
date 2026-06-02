0281c527bbdb4fad70b0de5565b48e193d8a7cb1
antoniobrandi <antonio.brandi@pal-robotics.com>
2026-06-02 10:23:01 +0200

    stores_server log level

--- tiago_gazebo/launch/navigation_private_sim.launch.py
+++ tiago_gazebo/launch/navigation_private_sim.launch.py
@@ -210,10 +210,13 @@ def declare_actions(
         namespace=LaunchConfiguration('namespace'),
         package='pal_stores_server',
         executable='pal_stores_server',
-        arguments=[PathJoinSubstitution([
-            os.environ['HOME'], '.pal',
-            PythonExpression(["'", LaunchConfiguration('namespace'), "stores.db'"]),
-        ])],
+        arguments=[
+            PathJoinSubstitution([
+                os.environ['HOME'], '.pal',
+                PythonExpression(["'", LaunchConfiguration('namespace'), "stores.db'"]),
+            ]),
+            '--ros-args', '--log-level', 'WARN'
+        ],
         condition=IfCondition(LaunchConfiguration('advanced_navigation'))
     )
 
